ca705842747289229d1ce19e319bcbf6c2965c48
muxamedmustaf <muxamedmustaf184@gmail.com>
2026-08-04 13:51:05 +0300

    Update ema.py

--- indicators/ema.py
+++ indicators/ema.py
@@ -1,1 +1,6 @@
+import pandas as pd
 
+def calculate_ema(df: pd.DataFrame, period: int = 50) -> pd.Series:
+    """Xisaabinta Exponential Moving Average"""
+    return df['close'].ewm(span=period, adjust=False).mean()
+  
