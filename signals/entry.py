2411bfc12122db4d74df6ca08950c80b3290a5ab
muxamedmustaf <muxamedmustaf184@gmail.com>
2026-08-04 15:22:11 +0300

    Update entry.py

--- signals/entry.py
+++ signals/entry.py
@@ -1,1 +1,5 @@
-
+def check_entry_conditions(df, current_price, current_ema):
+    """Go'aaminta goorta la galayo suuqa iyadoo la sugayo shamacu xidhmo"""
+    # Xaqiijinta in qiimuhu ka baxay ama ku jiro xaalad sax ah
+    return True
+  
