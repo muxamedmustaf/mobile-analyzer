beff2c6fae0d355fb0e98d6350e34f330c260be7
muxamedmustaf <muxamedmustaf184@gmail.com>
2026-08-04 13:26:00 +0300

    Update market_data.py

--- market_data.py
+++ market_data.py
@@ -1,1 +1,23 @@
+import ccxt
+import pandas as pd
 
+exchange = ccxt.binance()
+
+def get_data(symbol="BTC/USDT", timeframe="15m", limit=200):
+    ohlcv = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
+
+    df = pd.DataFrame(
+        ohlcv,
+        columns=[
+            "Time",
+            "Open",
+            "High",
+            "Low",
+            "Close",
+            "Volume"
+        ]
+    )
+
+    df["Time"] = pd.to_datetime(df["Time"], unit="ms")
+
+    return df
