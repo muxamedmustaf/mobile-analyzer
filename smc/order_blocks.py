5e3eb683a81fcb6d5e0369b324ebd2ba4d14baa6
muxamedmustaf <muxamedmustaf184@gmail.com>
2026-08-04 15:11:10 +0300

    Update order_blocks.py

--- smc/order_blocks.py
+++ smc/order_blocks.py
@@ -1,1 +1,15 @@
+import pandas as pd
 
+def detect_order_blocks(df: pd.DataFrame) -> list:
+    """Raadinta Bullish iyo Bearish Order Blocks ee suuqa"""
+    order_blocks = []
+    for i in range(2, len(df)):
+        # Tusaale hordhac ah: Shamaca ka horreeya dhaqdhaqaaqa xoogan
+        if df['close'].iloc[i] > df['open'].iloc[i] and df['close'].iloc[i-1] < df['open'].iloc[i-1]:
+            order_blocks.append({
+                "index": i-1,
+                "type": "BULLISH_OB",
+                "price": df['low'].iloc[i-1]
+            })
+    return order_blocks
+  
