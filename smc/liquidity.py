d64311b4402c313337c1c19ac5879b2ff10c46a8
muxamedmustaf <muxamedmustaf184@gmail.com>
2026-08-04 15:13:58 +0300

    Create liquidity.py

---
+++ smc/liquidity.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def detect_liquidity(df: pd.DataFrame) -> list:
+    """Aqoonsashada meelaha liquidity-ga (Equal Highs / Lows)"""
+    liquidity_zones = []
+    # Qorshaha hordhaca ah ee dareeraha suuqa
+    return liquidity_zones
+  
