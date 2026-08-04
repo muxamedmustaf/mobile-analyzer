c687d1c3d4d58c2907b106f6eb6ebd0118bee88a
muxamedmustaf <muxamedmustaf184@gmail.com>
2026-08-04 15:02:16 +0300

    Update bos.py

--- structure/bos.py
+++ structure/bos.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+def detect_bos(df: pd.DataFrame) -> pd.DataFrame:
+    """Raadinta Break of Structure (BOS) ee suuqa"""
+    df['bos'] = 0
+    # Qorshaha hordhaca ah ee BOS
+    return df
+  
