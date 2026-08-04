301a1c3d1857f9f6d1c3e2345d22bb0de0ba0687
muxamedmustaf <muxamedmustaf184@gmail.com>
2026-08-04 15:12:49 +0300

    Update fvg.py

--- smc/fvg.py
+++ smc/fvg.py
@@ -1,1 +1,17 @@
 
+import pandas as pd
+
+def detect_fvg(df: pd.DataFrame) -> list:
+    """Raadinta Fair Value Gaps (FVG) ee bannaan"""
+    fvgs = []
+    for i in range(1, len(df) - 1):
+        # Bullish FVG
+        if df['low'].iloc[i+1] > df['high'].iloc[i-1]:
+            fvgs.append({
+                "index": i,
+                "type": "BULLISH_FVG",
+                "bottom": df['high'].iloc[i-1],
+                "top": df['low'].iloc[i+1]
+            })
+    return fvgs
+  
