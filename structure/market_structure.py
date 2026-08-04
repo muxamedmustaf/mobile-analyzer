1f31c6e6313a19376e9f5b38fec6b7f8cb02818a
muxamedmustaf <muxamedmustaf184@gmail.com>
2026-08-04 15:04:31 +0300

    Update market_structure.py

--- structure/market_structure.py
+++ structure/market_structure.py
@@ -1,1 +1,16 @@
+from structure.swings import detect_swings
+from structure.trend import determine_trend
 
+class MarketStructureAnalyzer:
+    def __init__(self, df):
+        self.df = df
+
+    def analyze(self):
+        """Isku xidhka falanqaynta Swings iyo Trend-ka"""
+        df_swings = detect_swings(self.df)
+        trend = determine_trend(self.df)
+        return {
+            "trend": trend,
+            "data": df_swings
+        }
+      
