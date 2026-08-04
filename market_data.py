34676d075e2c1e68ce1ba3e84776b7ced9f1068d
muxamedmustaf <muxamedmustaf184@gmail.com>
2026-08-04 15:50:11 +0300

    Update market_data.py

--- market_data.py
+++ market_data.py
@@ -1,23 +1,19 @@
 import ccxt
 import pandas as pd
 
-exchange = ccxt.binance()
-
-def get_data(symbol="BTC/USDT", timeframe="15m", limit=200):
-    ohlcv = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
-
+def get_data(symbol="BTC/USDT", timeframe="30m", limit=100):
+    # Isticmaal CoinEx halkii ay ka noqon lahayd Binance
+    exchange = ccxt.coinex()
+    
+    # CoinEx magacyada suuqa waxay isticmaalaan hab aan lahayn slash (tusaale: BTCUSDT)
+    clean_symbol = symbol.replace("/", "")
+    
+    ohlcv = exchange.fetch_ohlcv(clean_symbol, timeframe=timeframe, limit=limit)
+    
     df = pd.DataFrame(
         ohlcv,
-        columns=[
-            "Time",
-            "Open",
-            "High",
-            "Low",
-            "Close",
-            "Volume"
-        ]
+        columns=["Time", "Open", "High", "Low", "Close", "Volume"]
     )
-
-    df["Time"] = pd.to_datetime(df["Time"], unit="ms")
-
+    df["Time"] = pd.to_datetime(df["Time"], unit='ms')
     return df
+    
